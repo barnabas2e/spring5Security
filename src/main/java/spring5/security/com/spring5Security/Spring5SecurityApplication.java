2cb0cbe8371669228f2e580786cd13ea2bc2cc7c
Barnabas Ebadan <barnabas2e@gmail.com>
2019-08-05 21:17:04 -0700

    Initial commit

---
+++ src/main/java/spring5/security/com/spring5Security/Spring5SecurityApplication.java
@@ -0,0 +1,13 @@
+package spring5.security.com.spring5Security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5SecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring5/security/com/spring5Security/Spring5SecurityApplicationTests.java
@@ -0,0 +1,16 @@
+package spring5.security.com.spring5Security;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring5SecurityApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
